a94fa911c9e3c314529bd947aefe6795520b9cb5
Nathan Casarez <nathanweb.dev@proton.me>
2025-12-19 17:35:53 -0800

    fixes.

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const authController = require('../controllers/auth') 
+const authController = require('../controllers/auth')
 const homeController = require('../controllers/home')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-router.get('/api/dashboard', ensureAuth, homeController.getIndex)
+router.get('/api/dashboard', homeController.getIndex)
 router.get('/api/google', authController.getLogin)
-router.get('/api/google/callback', authController.postLogin, authController.googleCallback)
+router.get(
+  '/api/google/callback',
+  authController.postLogin,
+  authController.googleCallback
+)
 router.get('/api/logout', authController.logout)
 
 module.exports = router
