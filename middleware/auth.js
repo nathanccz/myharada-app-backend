46c26b6c11ccda84e452871d388ace0206d30435
Nathan Casarez <nathanweb.dev@proton.me>
2025-12-19 21:42:33 -0800

    fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,10 @@
 module.exports = {
   ensureAuth: function (req, res, next) {
     if (req.isAuthenticated()) {
+      console.log('AUTHENTICATED')
       return next()
     } else {
+      console.log('NOT AUTHENTICATED')
       res.redirect('https://myharada.netlify.app')
     }
   },
