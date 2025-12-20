057c43b6e9ef12840573045fce1fe8ea044cc73f
Nathan Casarez <nathanweb.dev@proton.me>
2025-12-19 17:07:56 -0800

    url fixes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
 module.exports = {
-    ensureAuth: function (req, res, next) {
-        if (req.isAuthenticated()) {
-            return next()
-        } else {
-            res.redirect('http://localhost:5173/')
-        }
-    },
-    ensureGuest: function (req, res, next) {
-        if (req.isAuthenticated()) {
-            res.redirect('http://localhost:5173/dashboard')
-        } else {
-            return next()
-        }
-    },
+  ensureAuth: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    } else {
+      res.redirect('https://myharada.netlify.app//')
+    }
+  },
+  ensureGuest: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      res.redirect('https://myharada.netlify.app/dashboard')
+    } else {
+      return next()
+    }
+  },
 }
