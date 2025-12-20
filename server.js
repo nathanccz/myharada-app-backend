064f37090dc36c6d40101e518525a48c7e25dc5c
Nathan Casarez <nathanweb.dev@proton.me>
2025-12-19 21:33:21 -0800

    cookie fix

--- server.js
+++ server.js
@@ -56,7 +56,8 @@ app.use(
       touchAfter: 24 * 3600, // lazy session update
     }),
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
+      sameSite: 'none',
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
     },
