f5ff71689c999b8a7ebdbf66cb2edb7abaacab45
Nathan Casarez <nathanweb.dev@proton.me>
2025-12-21 15:59:12 -0800

    proxy fix

--- routes/main.js
+++ routes/main.js
@@ -7,7 +7,7 @@ const { ensureAuth, ensureGuest } = require('../middleware/auth')
 router.get('/api/auth/dashboard', ensureAuth, homeController.getIndex)
 router.get('/auth/google', authController.getLogin)
 router.get(
-  '/auth/google/callback',
+  'api/auth/google/callback',
   authController.postLogin,
   authController.googleCallback
 )
