bad30e7f0b970e2da40e127706f5e515f1c6d259
Nathan Casarez <nathanweb.dev@proton.me>
2026-01-14 13:07:31 -0800

    added api/health endpoint for the front-end to check if server is active on Render

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,8 @@
 module.exports = {
+  getHealth: (req, res) => {
+    console.log('waking up')
+    res.send('ok')
+  },
   getIndex: (req, res) => {
     return res.json(req.user)
   },
--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,7 @@ const homeController = require('../controllers/home')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 const { authenticate } = require('../middleware/firebaseAuth')
 
+router.get('/api/health', homeController.getHealth)
 router.get('/api/auth/dashboard', authenticate, homeController.getIndex)
 router.get('/auth/google', authController.getLogin)
 router.get('/api/auth/firebase-login', authenticate, authController.handleLogin)
